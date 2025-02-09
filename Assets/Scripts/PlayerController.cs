64f7ca515262cf8b7245525c824b4d077eee6d6f
up2273222 <up2273222@myport.ac.uk>
2025-02-09 17:17:42 +0000

    Nose throw + trajectory visualisation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -25,8 +26,12 @@ public class PlayerController : MonoBehaviour
     
     //Clown nose throw variables
     public GameObject clownNose;
+    private Rigidbody Nose_rb;
     public Transform throwTarget;
     public int arcMaximumHeight = 10;
+    public bool throwInitiated;
+    public bool throwCompleted;
+    public LineRenderer trajectoryRenderer;
     
     
     
@@ -42,6 +47,8 @@ public class PlayerController : MonoBehaviour
         //Assign components to variables
         _rb = GetComponent<Rigidbody>();
         _material = GetComponent<Renderer>().material;
+        trajectoryRenderer = GetComponent<LineRenderer>();
+      //  Nose_rb = clownNose.GetComponent<Rigidbody>();
         canMove = true;
     }
 
@@ -76,8 +83,43 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            throwNose();
+            if (!throwInitiated)
+            {
+                if (Nose_rb == null)
+                {
+                    throwInitiated = true;
+                    throwCompleted = false;
+                    GameObject Nose = Instantiate(clownNose, transform.position, Quaternion.identity);
+                    Nose_rb = Nose.GetComponent<Rigidbody>();
+                }
+                
+            }
+            else if (throwInitiated)
+            {
+                throwCompleted = true;
+            }
+
+        }
+
+        if (throwInitiated && !throwCompleted)
+        {
+            Nose_rb.useGravity = false;
             
+            Ray targetRay;
+            RaycastHit targetHit;
+            targetRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(targetRay, out targetHit))
+            {
+                throwTarget.position = new Vector3(targetHit.point.x, targetHit.point.y + 0.2f, targetHit.point.z);
+            }
+            Nose_rb.position = transform.position;
+            DrawNoseTrajectory(Nose_rb.position,CalculateThrowVelocity(throwTarget,Nose_rb) );
+        }
+        else if (throwInitiated && throwCompleted)
+        {
+            print("WAAA");
+            Nose_rb.useGravity = true;
+            throwNose(Nose_rb);
         }
         
 
@@ -110,27 +152,11 @@ public class PlayerController : MonoBehaviour
     
     //Handle clown nose throwing
 
-    void throwNose()
+    void throwNose(Rigidbody Nose_rb)
     {
-        GameObject Nose = Instantiate(clownNose, transform.position, Quaternion.identity);
-        Rigidbody Nose_rb = Nose.GetComponent<Rigidbody>();
-
-        Ray targetRay;
-        RaycastHit targetHit;
-        
-        targetRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(targetRay, out targetHit))
-        {
-            throwTarget.position = new Vector3(targetHit.point.x, targetHit.point.y + 0.2f, targetHit.point.z);
-            Nose_rb.velocity = CalculateThrowVelocity(throwTarget, Nose_rb);
-        }
-        
-       
-
-        
-
-
-
+        Nose_rb.velocity = CalculateThrowVelocity(throwTarget, Nose_rb);
+        throwInitiated = false;
+        throwCompleted = false;
     }
     
     Vector3 CalculateThrowVelocity(Transform target, Rigidbody Nose_rb)
@@ -142,6 +168,24 @@ public class PlayerController : MonoBehaviour
         Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2*arcMaximumHeight/Physics.gravity.y) + Mathf.Sqrt(2*(displacementY - arcMaximumHeight)/Physics.gravity.y));
 
         return velocityXZ + velocityY;
+    }
+
+    void DrawNoseTrajectory(Vector3 startPosition, Vector3 initialVelocity)
+    {
+        trajectoryRenderer.positionCount = 100;
+        float travelTime = 1.5f;
+        Vector3 currentPosition = startPosition;
+        Vector3 currentVelocity = initialVelocity;
+        float timeStep = travelTime/trajectoryRenderer.positionCount;
+
+        for (int i = 0; i < trajectoryRenderer.positionCount; i++)
+        {
+            trajectoryRenderer.SetPosition(i, currentPosition);
+            currentVelocity += new Vector3(0, Physics.gravity.y * timeStep, 0);
+            currentPosition += currentVelocity * timeStep;
+
+        }
+
     }
     
     
