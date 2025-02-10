b98e7d1c2124a35b845224af88593e714048f88b
up2273222 <up2273222@myport.ac.uk>
2025-02-10 17:12:08 +0000

    Nose now squeaks

--- Assets/Scripts/ClownNose.cs
+++ Assets/Scripts/ClownNose.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ClownNose : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip honk;
+    public bool hasCollided;
+    
    void OnCollisionEnter(Collision collision)
    {
-      
-      
-          //play squeak
-          print("Collision");
-          StartCoroutine(DestroyNose());
-      
-     
+       if (!hasCollided)
+       {
+           audioSource.PlayOneShot(honk);
+           hasCollided = true;
+           print("Collision");
+           StartCoroutine(DestroyNose());
+           Vector3 collisionPoint = collision.contacts[0].point;
+       }
+          
+
+
    }
 
 
