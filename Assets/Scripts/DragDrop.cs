84b262d957491bb3bca5d0ce2c09384964350115
ReaperBoy11 <vlady.vlasceanu@gmail.com>
2025-02-17 13:45:09 +0000

    Photo puzzle
    New scene with the photo puzzle drag and drop style

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left-click
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit) && hit.transform == transform)
+            {
+                isDragging = true;
+                offset = transform.position - hit.point;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        if (isDragging)
+        {
+            MoveObject();
+        }
+    }
+
+    private void MoveObject()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, transform.position);
+
+        if (plane.Raycast(ray, out float distance))
+        {
+            transform.position = ray.GetPoint(distance) + offset;
+        }
+    }
+}
