b9fc30e5e7aa8008499706be168989c90297cb38
up2273222 <up2273222@myport.ac.uk>
2025-03-20 11:27:10 +0000

    Add map button functionality

---
+++ Assets/Scripts/MapFastTravel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapFastTravel : MonoBehaviour
+{
+ public void LoadArea1()
+ {
+  print("Area 1 loaded");
+ }
+    
+    
+    
+}
